a3e62fa25a25930a3c8554cb7559cf0d2b04a218
Kevin Chan <chessmasterhong@yahoo.com>
2014-02-25 18:40:40 -0500

    Added PHP solution to problem 1.

---
+++ php/p1_kevin.php
@@ -0,0 +1,18 @@
+<?php
+
+/*  Problem 1: Multiples of 3 and 5
+ *  -----
+ *  If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ *  get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ *  Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+$s = 0;
+for($i = 0; $i < 1000; $i++)
+    if($i % 3 == 0 || $i % 5 == 0)
+        $s += $i;
+
+echo $s;
+
+?>
