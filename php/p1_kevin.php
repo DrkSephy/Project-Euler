66aa0764e8b4a4350e53063c4c7f26b440b2d3c7
Kevin Chan <chessmasterhong@yahoo.com>
2014-02-25 19:13:15 -0500

    Added PHP solution to problem 2. Minor fix to check for data type in problem 1.

--- php/p1_kevin.php
+++ php/p1_kevin.php
@@ -8,9 +8,10 @@
  *  Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
+// loop + mod div + accum
 $s = 0;
 for($i = 0; $i < 1000; $i++)
-    if($i % 3 == 0 || $i % 5 == 0)
+    if($i % 3 === 0 || $i % 5 === 0)
         $s += $i;
 
 echo $s;
