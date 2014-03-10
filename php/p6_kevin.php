d35c2fdcd8e1f9ec8c879a8000a5f37d53e21b6f
Kevin Chan <chessmasterhong@yahoo.com>
2014-03-09 22:04:16 -0400

    Added PHP solution to problem 6. Revised problem 1.

---
+++ php/p6_kevin.php
@@ -0,0 +1,20 @@
+<?php
+
+/*  The sum of the squares of the first ten natural numbers is,
+ *      1^2 + 2^2 + ... + 10^2 = 385
+ *  
+ *  The square of the sum of the first ten natural numbers is,
+ *      (1 + 2 + ... + 10)^2 = 552 = 3025
+ *  
+ *  Hence the difference between the sum of the squares of the first ten natural
+ *  numbers and the square of the sum is 3025 − 385 = 2640.
+ *  
+ *  Find the difference between the sum of the squares of the first one hundred
+ *  natural numbers and the square of the sum.
+*/
+
+for($sumOfSq = 0, $i = 1; $i <= 100; $i++) $sumOfSq += pow($i, 2);
+for($sqOfSums = 0, $j = 1; $j <= 100; $j++) $sqOfSums += $j;
+echo pow($sqOfSums, 2) - $sumOfSq;
+
+?>
