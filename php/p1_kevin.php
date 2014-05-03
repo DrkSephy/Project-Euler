b5d2d962b17cff421d01d027f0eae2e4d579e745
Kevin Chan <chessmasterhong@yahoo.com>
2014-05-02 23:26:54 -0400

    Optimized time complexity for problem 1 in PHP.

--- php/p1_kevin.php
+++ php/p1_kevin.php
@@ -8,7 +8,11 @@
  *  Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
-for($s = 0, $i = 1000; --$i;) $s += !($i % 3 && $i % 5) ? $i : 0;
+//for($s = 0, $i = 1000; --$i;) $s += !($i % 3 && $i % 5) ? $i : 0; // O(n)
+
+$s = 0;
+for($i = 0; $i < 1000; $i += 3) $s += $i; // O(n/3)
+for($i = 0; $i < 1000; $i += 5) $s += $i % 3 ? $i : 0; // O(n/5)
 echo $s;
 
 ?>
